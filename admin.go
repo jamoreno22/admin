849c47772921e9d133ba721dc53ae44aa66cac85
barbaraUribeCataldo <barbara.uribe@sansano.usm.cl>
2021-01-07 01:09:04 -0300

    debuggg

--- admin.go
+++ admin.go
@@ -63,8 +63,8 @@ func main() {
 		}
 		split := strings.Split(command, " ")
 		split2 := strings.Split(split[1], ".")
-		log.Println(len(split))
-		log.Println(len(split2))
+		log.Println(split)
+		log.Println(split2)
 		switch split[0] {
 		case "Create":
 			comm = l3.Command{Action: 1, Name: split2[0], Domain: split2[1],
